71fa9dedaa9f6964e8819ae38ff0f263967313fe
Jesse <jesse.scaramuzzino@Hotmail.com>
2024-12-15 01:05:19 +1100

    coding exercise

--- src/coding_exercise/application/splitter.py
+++ src/coding_exercise/application/splitter.py
@@ -2,13 +2,136 @@ from coding_exercise.domain.model.cable import Cable
 
 
 class Splitter:
+    
+    MINTIMES = 1
+    MAXTIMES = 64
+    MINLENGTH = 2
+    MAXLENGTH = 1024
+    
+    TENCABLES = 10
+    HUNDREDCABLES = 100
 
-    def __validate(self):
+    def __validate(self, cable: Cable, times: int):
+        """validates all inputs before proceeding with calculations
+
+        Args:
+            cable (Cable): cable object
+            times (int): number of times to be cut
+
+        Raises:
+            ValueError: cable length between 2 and 1024 inclusive
+            ValueError: cut amount less than length of cable
+            ValueError: cuts between 1 and 64
+        """
         valid = True
+        valid = self._validate_cable_length(cable.length)
         if not valid:
-            raise ValueError
+            raise ValueError(f"Cable not between {self.MINLENGTH} and {self.MAXLENGTH} inclusive.")
+        valid = self._validate_times(times)
+        if not valid:
+            raise ValueError(f"Cuts not between {self.MINTIMES} and {self.MAXTIMES} inclusive.")
+        valid = self._validate_enacted_cuts(cable.length, times)
+        if not valid:
+            raise ValueError(f"Amount of cuts will result in < 1 length cables. Can not cut.")
 
+    def _validate_times(self, times: int) -> bool:
+        return times >= self.MINTIMES and times <= self.MAXTIMES
+    
+    def _validate_enacted_cuts(self, cable_length: int, times: int) -> bool:
+        return cable_length > times 
+
+    def _validate_cable_length(self, cable_length: int) -> bool:
+        return cable_length >= self.MINLENGTH and cable_length <= self.MAXLENGTH
+    
+    @staticmethod
+    def plus_one(number: int) -> int:
+        one = 1
+        if isinstance(number, int):
+            return number + one
+        raise ValueError(f"You did not enter an int.")
+    
+    def get_new_cable_lengths(self, cable: Cable, times: int) -> list[int]:
+        """
+        Divide the cable into equal n + 1 lengths. Use the remainder to
+        get as many of the newely cut lengths as possible. Return all the 
+        lengths as a list
+
+        Args:
+            cable (Cable): cable object
+            times (int): amount of times to be cut
+
+        Returns:
+            list[int]: list of lengths
+        """
+        remainder_cables = []
+        number_of_main_new_cables = times + 1
+        length_of_main_new_cables = cable.length // number_of_main_new_cables
+        cables = [length_of_main_new_cables] * number_of_main_new_cables
+        
+        remainder = cable.length % number_of_main_new_cables
+        if remainder == 0:
+            return cables
+        
+        number_of_remainder_cable = remainder // length_of_main_new_cables
+        length_of_remainder_cable = remainder % length_of_main_new_cables
+        
+        if number_of_remainder_cable > 0:
+            remainder_cables = ([length_of_main_new_cables] * number_of_remainder_cable)
+        
+        if length_of_remainder_cable > 0:
+            remainder_cables.extend([length_of_remainder_cable])
+        
+        cables.extend(remainder_cables)
+        return cables   
+        
+    def get_zfill(self, length: int) -> int:
+        if length < self.TENCABLES:
+            return 1
+        elif length < self.HUNDREDCABLES:
+            return 2
+        else:
+            return 3
+    
+    def create_cables(self, cable_lengths: list[int]) -> list[Cable]:
+        """Create a list of cables with lengths and names
+
+        Args:
+            cable_lengths (list[int]): list of lengths
+
+        Returns:
+            list[Cable]: list of cables with zfilled names
+        """
+        cables = []
+        zfill_amount = self.get_zfill(len(cable_lengths))
+        for index, cable_length in enumerate(cable_lengths):
+            cable_number = str(index).zfill(zfill_amount)
+            cable = Cable(cable_length, f'coconuts-{cable_number}')
+            cables.append(cable)
+        return cables
+    
     def split(self, cable: Cable, times: int) -> list[Cable]:
-        self.__validate()
+        self.__validate(cable, times)
+        cables_lengths = self.get_new_cable_lengths(cable, times)
+        return self.create_cables(cables_lengths)
+        
+        
+if __name__ == "__main__":
+    given_cable = Cable(10, "coconuts")
+    result = Splitter().split(given_cable, 1)
+    for c in result:
+        print (c.name, c.length)
 
-        return []
+    given_cable = Cable(5, "coconuts")
+    result = Splitter().split(given_cable, 2)
+    for c in result:
+        print (c.name, c.length)
+    
+    given_cable = Cable(14, "coconuts")
+    result = Splitter().split(given_cable, 3)
+    for c in result:
+        print (c.name, c.length)
+    
+    given_cable = Cable(1000, "coconuts")
+    result = Splitter().split(given_cable, 50)
+    for c in result:
+        print (c.name, c.length)
--- tests/coding_exercise/application/test_splitter.py
+++ tests/coding_exercise/application/test_splitter.py
@@ -6,3 +6,72 @@ from coding_exercise.domain.model.cable import Cable
 
 def test_should_not_return_none_when_splitting_cable():
     assert_that(Splitter().split(Cable(10, "coconuts"), 1)).is_not_none()
+
+def test_should_raise_error_if_cuts_greater_than_cable():
+    cable = Cable(10, "coconuts")
+    assert_that(Splitter().split).raises(ValueError).when_called_with(cable, 11)
+
+def test_should_raise_error_if_cuts_less_than_min():
+    cable = Cable(10, "coconuts")
+    assert_that(Splitter().split).raises(ValueError).when_called_with(cable, -1)
+
+def test_should_raise_error_if_cuts_greater_than_max():
+    cable = Cable(10, "coconuts")
+    assert_that(Splitter().split).raises(ValueError).when_called_with(cable, 65)
+
+def test_should_raise_error_if_cable_length_less_than_min():
+    cable = Cable(1, "coconuts")
+    assert_that(Splitter().split).raises(ValueError).when_called_with(cable, 2)
+
+def test_should_raise_error_if_cable_length_greater_than_max():
+    cable = Cable(1025, "coconuts")
+    assert_that(Splitter().split).raises(ValueError).when_called_with(cable, 2)
+
+def test_should_raise_error_if_nan():
+    assert_that(Splitter().plus_one).raises(ValueError).when_called_with('one')
+
+def test_should_equal_no_remainder():
+    given_cable = Cable(10, "coconuts")
+    assert_that(Splitter().get_new_cable_lengths(given_cable, 1)).is_equal_to([5, 5])
+
+def test_should_equal_with_remainder():
+    given_cable = Cable(5, "coconuts")
+    assert_that(Splitter().get_new_cable_lengths(given_cable, 2)).is_equal_to([1, 1, 1, 1, 1])
+
+def test_should_equal_plus_one():
+    assert_that(Splitter().plus_one(1)).is_equal_to(2)
+
+def test_should_equal_zfill_1():
+    assert_that(Splitter().get_zfill(9)).is_equal_to(1)
+
+def test_should_equal_zfill_2():
+    assert_that(Splitter().get_zfill(99)).is_equal_to(2)
+
+def test_should_equal_zfill_3():
+    assert_that(Splitter().get_zfill(999)).is_equal_to(3)
+
+def test_should_have_correct_names_under_ten_cables():
+    given_cable = Cable(5, "coconuts")
+    split_results = Splitter().split(given_cable, 2)
+    split_names = [cable.name for cable in split_results]
+    expected_results = ['coconuts-0', 'coconuts-1', 'coconuts-2', 'coconuts-3', 'coconuts-4']
+    assert_that(split_names).is_equal_to(expected_results)
+    
+def test_should_have_correct_names_over_ten_cables():
+    given_cable = Cable(20, "coconuts")
+    split_results = Splitter().split(given_cable, 9)
+    split_names = [cable.name for cable in split_results]
+    expected_results = [
+        'coconuts-00',
+        'coconuts-01',
+        'coconuts-02',
+        'coconuts-03',
+        'coconuts-04',
+        'coconuts-05',
+        'coconuts-06',
+        'coconuts-07',
+        'coconuts-08',
+        'coconuts-09'
+    ]
+    assert_that(split_names).is_equal_to(expected_results)
+    
